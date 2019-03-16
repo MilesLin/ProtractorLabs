117d1f23bd3550beb5e333a4e0325ad6a26b706d
Miles <miles@miniasp.com>
2019-03-16 15:54:54 +0800

    練習複雜 DOM 定位運用 使用 filter 選擇搜尋結果

--- e2e/src/events/search-event.e2e-spec.ts
+++ e2e/src/events/search-event.e2e-spec.ts
@@ -28,7 +28,10 @@ fdescribe('search an event', () => {
 
   it('should click the second search result', async () => {
 
-    await element(by.className('list-group')).all(by.tagName('a')).get(1).click();
+    await element(by.className('list-group')).all(by.tagName('a'))
+    .filter(elem => {
+      return elem.getText().then(x => x.includes('實戰開發'));
+    }).first().click();
 
     // 驗證第一個 h2 是 「ANGULAR 7 開發實戰：新手入門篇」
     const text = await element(by.tagName('h2')).getText();
