56e4803f63dc02b86dd0b62928b3568e29cf0fe0
Miles <miles@miniasp.com>
2019-03-16 13:57:39 +0800

    登入失敗的 E2E 測試

--- e2e/src/user/login.e2e-spec.ts
+++ e2e/src/user/login.e2e-spec.ts
@@ -1,8 +1,11 @@
 import { browser, by, element } from 'protractor';
 
 describe('the user try to login', () => {
-  it('should login to event page', async () => {
+  beforeEach(async () => {
     await browser.get('/user/login');
+  });
+
+  it('should login to event page', async () => {
     await element(by.id('userName')).sendKeys('John');
     await element(by.id('password')).sendKeys('123456');
     await element(by.buttonText('登入')).click();
@@ -10,5 +13,13 @@ describe('the user try to login', () => {
     const url = await browser.getCurrentUrl();
     console.log('url: ' + url);
     expect(url).toBe(browser.baseUrl + 'events');
+  });
+
+  it('should show login failed when typing wrong password', async () => {
+    await element(by.id('userName')).sendKeys('John');
+    await element(by.id('password')).sendKeys('abc');
+    await element(by.buttonText('登入')).click();
+    const isAlertPresent = await element(by.className('alert-danger')).isPresent();
+    expect(isAlertPresent).toBe(true, '錯誤密碼案例驗證失敗');
   });
 });
