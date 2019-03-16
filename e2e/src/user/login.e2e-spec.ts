c9fbad0f16ad11c03601a860cceefc3c56fe312f
Miles <miles@miniasp.com>
2019-03-16 13:33:51 +0800

    Node.js 7.6.0+ 完整支援 async/await

--- e2e/src/user/login.e2e-spec.ts
+++ e2e/src/user/login.e2e-spec.ts
@@ -1,12 +1,14 @@
 import { browser, by, element } from 'protractor';
 
 describe('the user try to login', () => {
-  it('should login to event page', () => {
-    browser.get('/user/login');
-    element(by.id('userName')).sendKeys('John');
-    element(by.id('password')).sendKeys('123456');
-    element(by.buttonText('登入')).click();
-    browser.waitForAngular();
-    expect(browser.getCurrentUrl()).toContain('events');
+  it('should login to event page', async () => {
+    await browser.get('/user/login');
+    await element(by.id('userName')).sendKeys('John');
+    await element(by.id('password')).sendKeys('123456');
+    await element(by.buttonText('登入')).click();
+    await browser.waitForAngular();
+    const url = await browser.getCurrentUrl();
+    console.log('url: ' + url);
+    expect(url).toBe(browser.baseUrl + 'events');
   });
 });
